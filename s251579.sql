0feca0bd1898b733b20132ce4f34353e6c5ace90
androsanta <andreasantu@gmail.com>
2018-06-29 14:46:42 +0200

    minor fixes

--- s251579.sql
+++ s251579.sql
@@ -7,7 +7,8 @@ CREATE TABLE users (
 );
 
 
-INSERT INTO users VALUES ("u1@p.it", md5("p1"), "AA", "DD", 2);
+INSERT INTO users VALUES ("u1@p.it", md5("p1"), "FF", "KK", 4);
 INSERT INTO users VALUES ("u2@p.it", md5("p2"), "BB", "EE", 1);
 INSERT INTO users VALUES ("u3@p.it", md5("p3"), "DD", "EE", 1);
-INSERT INTO users VALUES ("u4@p.it", md5("p4"), NULL, NULL, 0);
+INSERT INTO users VALUES ("u4@p.it", md5("p4"), "AL", "DD", 1);
+INSERT INTO users VALUES ("u5@p.it", md5("p5"), NULL, NULL, 0);
